de5e8ab97d20561be05980ae3235a42901765f3a
Kyle <kyle.hanson@reputation.com>
2020-05-30 21:38:13 -0700

    initial commit

---
+++ background.js
@@ -0,0 +1,31 @@
+const spec = [ "blocking" ];
+const filter = {
+    urls: [
+        "*://content.googleapis.com/youtube/v3/channels?forUsername=*"
+    ]
+};
+
+const listener = req => {
+    console.info(`BEGIN redirector: ${req.url}`);
+
+    const cid = req.url.match(/(?<=forUsername=)[\w\d\-]+/)[0];
+
+    /**
+     * Making the assumption that any channelId that isn't 24 characters
+     * is a 'named' channel so redirection isn't needed.
+     */
+    if(cid.length !== 24) {
+        return;
+    }
+
+    const rurl = req.url;
+    const redirectUrl = rurl.replace('forUsername=', 'id=');
+
+    console.info(`Redirect to: ${redirectUrl}`);
+
+    return {
+        redirectUrl
+    };
+};
+
+chrome.webRequest.onBeforeRequest.addListener(listener, filter, spec);
